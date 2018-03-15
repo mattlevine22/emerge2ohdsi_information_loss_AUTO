bf1000971dc6d4e741a2ad8bfd2eadeab71f7420
Matt Levine <mattlevine22@gmail.com>
2018-03-15 16:12:37 -0400

    debugging R code

--- run_analysis.sh
+++ run_analysis.sh
@@ -10,5 +10,5 @@ psql $dbname $username -a -f src/condition_occurrence_descriptions.sql;
 psql $dbname $username -a -f src/long_run4_fake_mapped_all_descendants.sql;
 psql $dbname $username -a -f src/remap_run1.sql;
 psql $dbname $username -a -f src/output_src_code_descriptions_for_histograms.sql;
-R --vanilla < src/output_num_mappings_table.R ./output/emerge_final_icd_table.csv ./output;
+Rscript --vanilla src/output_num_mappings_table.R ./output/emerge_final_icd_table.csv ./output;
 
