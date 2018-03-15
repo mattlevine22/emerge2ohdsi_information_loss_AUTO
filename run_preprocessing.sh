08ecd7e3901782a09ed6721aedaa1ec1a74a8d40
Matt Levine <mattlevine22@gmail.com>
2018-03-15 13:34:51 -0400

    rename

--- run_preprocessing.sh
+++
@@ -1,9 +0,0 @@
-# Here is the workflow for pre-processing the terminology mapping data
-
-# GEM: /phi/proj/phenotype2008/emerge/omopdefinitions/eMERGE_concept_sets.csv
-
-# Convert to JSON style:
-bash src/PreProcessJSON_macOS.sh ./emerge_files/eMERGE_concept_sets.csv ./emerge_files/eMERGE_concept_sets_processed.json
-
-# Generate processed CSV with Python script
-python src/ProcessConceptSets.py ./emerge_files/eMERGE_concept_sets_processed.json ./emerge_files/eMERGE_concept_sets_processed.csv
--- run_preprocessing_linux.sh
+++ run_preprocessing_linux.sh
@@ -3,7 +3,7 @@
 # GEM: /phi/proj/phenotype2008/emerge/omopdefinitions/eMERGE_concept_sets.csv
 
 # Convert to JSON style:
-bash src/PreProcessJSON_macOS.sh ./emerge_files/eMERGE_concept_sets.csv ./emerge_files/eMERGE_concept_sets_processed.json
+bash src/PreProcessJSON_GNU.sh ./emerge_files/eMERGE_concept_sets.csv ./emerge_files/eMERGE_concept_sets_processed.json
 
 # Generate processed CSV with Python script
 python src/ProcessConceptSets.py ./emerge_files/eMERGE_concept_sets_processed.json ./emerge_files/eMERGE_concept_sets_processed.csv
